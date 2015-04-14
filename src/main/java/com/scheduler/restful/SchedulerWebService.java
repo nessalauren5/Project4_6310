bd547e3124744b2fb5405599c608ab6f6b31178b
Vanessa Anderson <nessa.lauren5@gmail.com>
2015-04-14 15:00:23 -0400

    add new webservice methods, course list and add course everything runs on server smoothly now

--- src/main/java/com/scheduler/restful/SchedulerWebService.java
+++ src/main/java/com/scheduler/restful/SchedulerWebService.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -29,6 +29,13 @@ public class SchedulerWebService {
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world, welcome to our scheduler web app!");
 		return "hello";
+	}
+
+	@GET
+	@Produces("application/json")
+	@Path("test")
+	public Response testService() {
+		return Response.ok().build();
 	}
 
 	@POST
