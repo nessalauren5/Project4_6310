1a230390d84105cfe2caf73a77e20fb139cb053e
Keith Adler <Talador12@gmail.com>
2015-04-20 21:53:00 -0500

    Student Course Catalog Table + additions to the priority list

--- WebContent/js/landingPage.js
+++ WebContent/js/landingPage.js
@@ -1,3 +1,54 @@
 $(document).ready(function(){
   $('.sortable-list').sortable();
 });
+
+function getRecommendedSchedule() {
+    if ((document.getElementById("recommendedSchedule").style.display == 'none'))
+    { 
+		//Call to retrieve list
+
+		//Populate list
+
+		//List Toggle
+        document.getElementById("recommendedSchedule").style.display = '' 
+        event.preventDefault()
+    }
+    else
+    { 
+    	//List Toggle
+        document.getElementById("recommendedSchedule").style.display = 'none'; 
+        event.preventDefault()
+    }
+}
+
+function addClass(x) {
+    var rowData = document.getElementById("course"+x);
+    var selectedText = rowData.cells[0].innerHTML + ': ' + rowData.cells[1].innerHTML;
+
+    if(checkClassDuplicate(selectedText)) //add a check to see if class is already taken and AND it here.
+    {
+	    var list = document.getElementById('priorityList');
+		var entry = document.createElement('li');
+		entry.setAttribute('class', 'sortable-item ui-sortable-handle')
+		var newSpan = document.createElement('span');
+		entry.appendChild(newSpan);
+		newSpan.innerHTML = selectedText;
+		list.appendChild(entry);
+	}
+}
+
+function checkClassDuplicate(x) {
+	var list = document.getElementById('priorityList');
+	var listItem = list.getElementsByTagName("span");
+	var newNums   = [],
+	    duplicate = [];
+
+	for (var i=0; i < listItem.length; i++) {
+	    var num = listItem[i].innerHTML;
+	    if (num === x) {
+	        alert("" + x + " is a duplicate class");
+	        return false;
+	    }
+	}
+	return true;
+}
