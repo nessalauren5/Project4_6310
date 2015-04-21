466968b54099ce2d98f8e3bc960af3bae760a5e9
Keith Adler <Talador12@gmail.com>
2015-04-21 12:35:08 -0500

    Added some of the other JS from before Added an ID to semester Added separate JS from admin/professor

--- WebContent/js/landingPage.js
+++ WebContent/js/landingPage.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
   $('.sortable-list').sortable();
 
-  $("#showRecommendedSchedule").click(
-      function (e) {
-        e.preventDefault();
-        getSchedule();
-
-      }
-  );
-
   var $loading = $('#loadingDiv').hide();
   $(document)
       .ajaxStart(function () {
@@ -109,11 +101,25 @@ $(document).ready(function(){
     }
   }
 
-  function getSchedule(){
+});
+
+function getRecommendedSchedule() {
+  if ((document.getElementById("recommendedSchedule").style.display == 'none'))
+  {
+    //Call to retrieve list
+    var courses = {};
+
+    $.each('.priority',function(key, course) {
+      courses.push(course.text);
+    });
+
+    var json = JSON.stringify({courseList:courses});
+    console.log(json);
 
     $.ajax({
       url : uri + "recc/" +  $.QueryString["id"] + "/" + $('#courseload').val(),
-      type : "GET",
+      type : "POST",
+      data: json,
       contentType: "application/json",
       dataType:"json",
       success : function(data) {
@@ -123,8 +129,54 @@ $(document).ready(function(){
         alert(" Can't do because: " + error);
       }
     });
+
+    //Populate list
+
+    //List Toggle
+    document.getElementById("recommendedSchedule").style.display = ''
+    event.preventDefault()
   }
+  else
+  {
+    //List Toggle
+    document.getElementById("recommendedSchedule").style.display = 'none';
+    event.preventDefault()
+  }
+}
 
+function addClass(x) {
+  var rowData = document.getElementById("course"+x);
+  var selectedText = rowData.cells[0].innerHTML + ': ' + rowData.cells[1].innerHTML;
 
+  if(checkClassDuplicate(selectedText)) //add a check to see if class is already taken and AND it here.
+  {
+    var list = document.getElementById('priorityList');
+    var entry = document.createElement('li');
+    entry.setAttribute('class', 'sortable-item ui-sortable-handle')
+    var newSpan = document.createElement('span');
+    entry.appendChild(newSpan);
+    newSpan.innerHTML = selectedText;
+    list.appendChild(entry);
+  }
+}
 
-});
+function checkClassDuplicate(x) {
+  var list = document.getElementById('priorityList');
+  var listItem = list.getElementsByTagName("span");
+  var newNums   = [],
+      duplicate = [];
+
+  for (var i=0; i < listItem.length; i++) {
+    var num = listItem[i].innerHTML;
+    if (num === x) {
+      alert("" + x + " is a duplicate class");
+      return false;
+    }
+  }
+  return true;
+}
+
+function getSem(sel) {
+  var sem = document.getElementById('semester');
+  sem.innerHTML = sel.value;
+}
